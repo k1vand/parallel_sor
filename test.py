e745c9a8df599f92d26a5ecc396f8dd2462b0bd4
kivand <ivanin.kirill00yandxe.ru>
2025-03-03 22:30:24 +0300

    final test with best case

--- test.py
+++ test.py
@@ -5,75 +5,85 @@ import numpy as np
 from string import Template
 from typing import Dict, Union, List
 import math
+import csv
+import time
 
-PARAM_ABS_MAX = 100
+from gen_linear_system import gen_linear_three_diagonal_system
 
 
 w = 1.5
-e = 0.0001
-ns = [3, 6, 12] # A sizes
-instance_nums = [1, 2, 4] # threads / procs num
+e = 0.000000000001
+ns = [(2000, 0.00001)]  # A sizes
+instance_nums = [1, 2, 3, 4, 6, 8]  # threads / procs num
 
 
 def module_path():
     return os.path.dirname(os.path.realpath(__file__))
 
-algs_paths = {
-        "c_pthread": os.path.join(module_path(), "c_pthreads", "build", "sor"),
-        "c_omp": os.path.join(module_path(), "c_omp", "build", "sor"),
-        "c_mpi": os.path.join(module_path(), "c_mpi", "build", "sor"),
-        "python_mpi": os.path.join(module_path(), "python_mpi", "python_mpi.py"),
-    }
 
-algs: List[Dict[str, Union[str,Template]]] = [
-    # {"name": "c_pthreads", "cmd": Template(f"{algs_paths['c_pthread']} -c $linsys_path -o $out_path -n $n -t $t -e $e -w $w")},
-    # {"name": "c_omp", "cmd": Template(f"{algs_paths['c_omp']} -c $linsys_path -o $out_path -n $n -t $t -e $e -w $w")},
-    {"name": "c_mpi", "cmd": Template(f"mpirun --use-hwthread-cpus -np $t {algs_paths['c_mpi']} -c $linsys_path -o $out_path -n $n -e $e -w $w")},
-    {"name": "python_mpi", "cmd": Template(f"mpirun --use-hwthread-cpus -np $t /usr/bin/python3 {algs_paths['python_mpi']} -c $linsys_path -o $out_path -n $n -e $e -w $w")}
+algs_paths = {
+    "c_pthread": os.path.join(module_path(), "c_pthreads", "build", "sor"),
+    "c_omp": os.path.join(module_path(), "c_omp", "build", "sor"),
+    "c_mpi": os.path.join(module_path(), "c_mpi", "build", "sor"),
+    "python_mpi": os.path.join(module_path(), "python_mpi", "python_mpi.py"),
+}
+
+algs: List[Dict[str, Union[str, Template]]] = [
+    {
+        "name": "c_pthreads",
+        "cmd": Template(
+            f"{algs_paths['c_pthread']} -c $linsys_path -o $out_path -n $n -t $t -e $e -w $w"
+        ),
+    },
+    {
+        "name": "c_omp",
+        "cmd": Template(
+            f"{algs_paths['c_omp']} -c $linsys_path -o $out_path -n $n -t $t -e $e -w $w"
+        ),
+    },
+    {
+        "name": "c_mpi",
+        "cmd": Template(
+            f"mpirun --use-hwthread-cpus -np $t {algs_paths['c_mpi']} -c $linsys_path -o $out_path -n $n -e $e -w $w"
+        ),
+    },
+    {
+        "name": "python_mpi",
+        "cmd": Template(
+            f"mpirun --use-hwthread-cpus -np $t /usr/bin/python3 {algs_paths['python_mpi']} -c $linsys_path -o $out_path -n $n -e $e -w $w"
+        ),
+    },
 ]
 
 
-
-def gen_linear_system(n: int):
-    A = np.zeros((n, n), dtype=int)
-    b = np.zeros((n), dtype=int)
-    
-    for i in range(0, n):
-        b[i] = np.random.randint(-PARAM_ABS_MAX, PARAM_ABS_MAX)
-        while A[i,i] == 0:
-            A[i, i] = np.random.randint(-PARAM_ABS_MAX, PARAM_ABS_MAX)
-        new_max = int(abs(A[i, i]) / (n - 1) - 1)
-
-        for j in range(n):
-            if i != j:
-                A[i, j] = random.randint(-new_max, new_max)  
-    
-    return (A, b)
-    
-
 def main():
+    result = list()
     linsys_dir = os.path.join(module_path(), "linsys")
     outs_dir = os.path.join(module_path(), "outs")
-    
-    
+    test_out = os.path.join(module_path(), "test_out.csv")
+    csv_writer = csv.DictWriter(
+        open(test_out, "w"),
+        fieldnames=["alg", "instances", "n", "elapsed_ms", "l2_norm", "relative_error"],
+    )
+    csv_writer.writeheader()
+
     os.makedirs(linsys_dir, exist_ok=True)
     os.makedirs(outs_dir, exist_ok=True)
 
-    np.random.seed(1234567890)
     linsys = list()
-    for i in range (0, len(ns)):
-        n = ns[i]
-        A, b = gen_linear_system(n)
+    for i in range(0, len(ns)):
+        n, rel = ns[i]
+        A, b = gen_linear_three_diagonal_system(n, rel)
         sol = np.linalg.solve(A, b)
         linsys.append((A, b, np.round(sol, decimals=int(abs(math.log10(e))))))
-        
+
         b = b.reshape(-1, 1)
         Ab = np.hstack((A, b))
-        linsys_path = os.path.join(linsys_dir, f"{n}.txt");
+        linsys_path = os.path.join(linsys_dir, f"{n}.txt")
         with open(linsys_path, "w") as f:
             for j in range(0, n):
                 f.write(" ".join(map(str, Ab[j].tolist())) + "\n")
-        
+
         print(f"A|b = \n{Ab}")
         print(f"n = {n}\n")
         for instance_num in instance_nums:
@@ -82,22 +92,47 @@ def main():
             print(f"instance num = {instance_num}\n")
 
             for alg in algs:
-                out_path = os.path.join(outs_dir, f"{alg['name']}_{n}_{instance_num}.txt")
-                cmd = alg["cmd"].substitute(linsys_path = linsys_path, out_path=out_path, n=n, t=instance_num, e=e, w=w)
-                
+                out_path = os.path.join(
+                    outs_dir, f"{alg['name']}_{n}_{instance_num}.txt"
+                )
+                cmd = alg["cmd"].substitute(
+                    linsys_path=linsys_path,
+                    out_path=out_path,
+                    n=n,
+                    t=instance_num,
+                    e=e,
+                    w=w,
+                )
+
                 print(f"Alg: {alg['name']}\n{cmd}")
-                result = subprocess.run(map(str, cmd.split()), stdout=subprocess.DEVNULL)
-                
+                start_timestamp = time.time()
+                result = subprocess.run(
+                    map(str, cmd.split()), stdout=subprocess.DEVNULL
+                )
+                elapsed_ms = (time.time() - start_timestamp) * 1000
                 if result.returncode == 0:
                     with open(out_path, "r") as f:
                         solution = [float(x) for x in f.readline().split()]
-                        print(f"{os.path.basename(out_path)}, solution \n{solution}")
-                        print(f"true solution \n{linsys[i][2].tolist()}")
+                        # print(f"{os.path.basename(out_path)}, solution \n{solution}")
+                        # print(f"true solution \n{linsys[i][2].tolist()}")
 
                         l2_norm = np.linalg.norm(solution - linsys[i][2], ord=2)
                         relative_error = l2_norm / np.linalg.norm(linsys[i][2], ord=2)
 
-                        print(f"l2_norm = {l2_norm}, relative_error = {relative_error}\n")
+                        print(
+                            f"l2_norm = {l2_norm}, relative_error = {relative_error}\n"
+                        )
+
+                        csv_writer.writerow(
+                            {
+                                "alg": alg["name"],
+                                "instances": instance_num,
+                                "n": n,
+                                "elapsed_ms": elapsed_ms,
+                                "l2_norm": l2_norm,
+                                "relative_error": relative_error,
+                            }
+                        )
 
 
 if __name__ == "__main__":
